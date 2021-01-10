ab3e97ea5a6c99891f6b5b88acf5c6d70369fe8a
Michael Papesca <michael.papesca@onmail.com>
2021-01-10 07:57:44 -0500

    Add packages for three.js in expo

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import 'react-native-gesture-handler';
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { THREE } from 'expo-three';
 
 import RootDrawer from './src/nav/RootDrawer';
 import LoadingScreen from './src/screens/LoadingScreen';
 
+global.THREE = THREE;
+
 export default function App() {
 
   const [isLoading, setIsLoading] = useState(true);
-  setTimeout(() => setIsLoading(false), 5000);
+  setTimeout(() => setIsLoading(false), 3000);
   return (
     <NavigationContainer>
       { isLoading
--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Text, View, StyleSheet } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
+import { THREE, Renderer } from 'expo-three';
+
+// Make sure THREE reference is set.
+global.THREE = global.THREE || THREE;
 
 export default function GameScreen() {
     return (
         <SafeAreaView>
-            <Text>
-                Game
-            </Text>
+            <GLView
+                style={{ flex: 1 }}
+                onContextCreate={(gl: ExpoWebGLRenderingContext) => {
+                    // Create a WebGLRenderer without a DOM element
+                    const renderer = new Renderer({ gl });
+                    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+                }}
+            />
         </SafeAreaView>
     );
 }
